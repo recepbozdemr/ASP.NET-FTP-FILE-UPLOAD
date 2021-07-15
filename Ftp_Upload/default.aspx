12b78caf11f1ebf2aa9ec57a19d9df88885fbb43
Recep Bozdemir <48210122+recepbozdemr@users.noreply.github.com>
2021-07-15 11:17:45 +0300

    Commit

---
+++ Ftp_Upload/default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Ftp_Upload._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+  <asp:FileUpload ID="FileUpload1"  runat="server" />
+<asp:Button Text="FTP'ye Yükle" runat="server" OnClick="FTPUpload" />
+<hr />
+<asp:Label ID="lblMessage" runat="server" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Ftp_Upload/default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Net;
+using System.Text;
+namespace Ftp_Upload
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void FTPUpload(object sender, EventArgs e)
+        {
+            //FTP Server Adresi
+            string ftp = "ftp://testsite.com/";
+
+            //FTP Klasör adı.Kök klasöre yüklemek istiyorsan boş bırak
+            string ftpFolder = "httpdocs/demo/";
+
+            byte[] fileBytes = null;
+
+           // Dosyayı okuyup Byte dizinine dönüştür
+            string fileName = Path.GetFileName(FileUpload1.FileName);
+            using (StreamReader fileStream = new StreamReader(FileUpload1.PostedFile.InputStream))
+            {
+                fileBytes = Encoding.UTF8.GetBytes(fileStream.ReadToEnd());
+                fileStream.Close();
+            }
+
+            try
+            {
+                //FTP Request oluşturuluyor
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftp + ftpFolder + fileName);
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+
+                //Ftp Kimlik Bilgileri
+                request.Credentials = new NetworkCredential("kullanıcı adı", "şifre");
+                request.ContentLength = fileBytes.Length;
+                request.UsePassive = true;
+                request.UseBinary = true;
+                request.ServicePoint.ConnectionLimit = fileBytes.Length;
+                request.EnableSsl = false;   //SSL varsa True Mode
+
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    requestStream.Write(fileBytes, 0, fileBytes.Length);
+                    requestStream.Close();
+                }
+
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+                lblMessage.Text += fileName + " uploaded.<br />";
+                response.Close();
+            }
+            catch (WebException ex)
+            {
+                throw new Exception((ex.Response as FtpWebResponse).StatusDescription);
+            }
+        }
+    }
+
+}
